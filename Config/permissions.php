f62ac1178b666c56b87d5940cf7998bcc4153646
Samuel Odhiambo <sodhiambo@collabmed.com>
2016-11-10 11:35:25 +0300

    Update permission model

---
+++ Config/permissions.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+return [
+    'reception.patients' => [
+        'add' => 'Add new patient',
+        'view' => 'View patients',
+        'manage' => 'Edit patient information',
+        'checkin' => 'Checkin a patient',
+        'checkins' => 'View checked in patients'
+    ],
+    'reception.appointments' => [
+        'view' => 'View appointments and calendar',
+        'create' => 'Create new appointments',
+        'edit' => 'Edit / Reschedule appointments',
+        'cancel' => 'Delete or Cancel apppointments',
+    ],
+    'reception.documents' => [
+        'view' => 'View patient Documents',
+        'upload' => 'Upload documents',
+        'delete' => 'Delete patient document'
+    ],
+    'reception.settings' => [
+        'view_categories' => 'View appointment categories',
+        'manage_categories' => 'Edit , Create and modify appointment categories',
+    ]
+];
