6a366953900e110c07997bb1d640b71716fb382d
Samuel Dervis <samueldervis@gmail.com>
2017-09-15 13:29:22 +0300

    Temporary commit

--- Entities/Appointments.php
+++ Entities/Appointments.php
@@ -9,67 +9,44 @@ use Carbon\Carbon;
 use Jenssegers\Date\Date;
 use MaddHatter\LaravelFullcalendar\Event;
 
-/**
- * Ignite\Reception\Entities\Appointments
- *
- * @property integer $id
- * @property integer $patient
- * @property string $guest
- * @property string $phone
- * @property \Carbon\Carbon $time
- * @property integer $category
- * @property integer $doctor
- * @property string $instructions
- * @property integer $status
- * @property integer $clinic
- * @property \Carbon\Carbon $created_at
- * @property \Carbon\Carbon $updated_at
- * @property-read \Ignite\Reception\Entities\Patients $patients
- * @property-read \Ignite\Users\Entities\UserProfile $doctors
- * @property-read \Ignite\Reception\Entities\AppointmentCategory $categories
- * @property-read \Ignite\Settings\Entities\Clinics $clinics
- * @property-read mixed $is_guest
- * @property-read mixed $is_future
- * @method static \Illuminate\Database\Query\Builder|\Ignite\Reception\Entities\Appointments whereId($value)
- * @method static \Illuminate\Database\Query\Builder|\Ignite\Reception\Entities\Appointments wherePatient($value)
- * @method static \Illuminate\Database\Query\Builder|\Ignite\Reception\Entities\Appointments whereGuest($value)
- * @method static \Illuminate\Database\Query\Builder|\Ignite\Reception\Entities\Appointments wherePhone($value)
- * @method static \Illuminate\Database\Query\Builder|\Ignite\Reception\Entities\Appointments whereTime($value)
- * @method static \Illuminate\Database\Query\Builder|\Ignite\Reception\Entities\Appointments whereCategory($value)
- * @method static \Illuminate\Database\Query\Builder|\Ignite\Reception\Entities\Appointments whereDoctor($value)
- * @method static \Illuminate\Database\Query\Builder|\Ignite\Reception\Entities\Appointments whereInstructions($value)
- * @method static \Illuminate\Database\Query\Builder|\Ignite\Reception\Entities\Appointments whereStatus($value)
- * @method static \Illuminate\Database\Query\Builder|\Ignite\Reception\Entities\Appointments whereClinic($value)
- * @method static \Illuminate\Database\Query\Builder|\Ignite\Reception\Entities\Appointments whereCreatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\Ignite\Reception\Entities\Appointments whereUpdatedAt($value)
- * @mixin \Eloquent
- */
-class Appointments extends Model implements Event {
+class Appointments extends Model implements Event
+{
 
     protected $dates = ['time'];
     public $table = 'reception_appointments';
 
-    public function patients() {
+    public function patients()
+    {
         return $this->belongsTo(Patients::class, 'patient');
     }
 
-    public function doctors() {
+    public function doctors()
+    {
         return $this->belongsTo(UserProfile::class, 'doctor', 'user_id');
     }
 
-    public function categories() {
+    public function categories()
+    {
         return $this->belongsTo(AppointmentCategory::class, 'category', 'id');
     }
 
-    public function clinics() {
+    public function getMobileAttribute()
+    {
+        return $this->phone ?: $this->patients->mobile;
+    }
+
+    public function clinics()
+    {
         return $this->belongsTo(Clinics::class, 'clinic');
     }
 
-    public function getIsGuestAttribute() {
+    public function getIsGuestAttribute()
+    {
         return empty($this->patient);
     }
 
-    public function getIsFutureAttribute() {
+    public function getIsFutureAttribute()
+    {
         return $this->time->gt(Carbon::now());
     }
 
@@ -78,7 +55,8 @@ class Appointments extends Model implements Event {
      *
      * @return int
      */
-    public function getId() {
+    public function getId()
+    {
         return $this->schedule_id;
     }
 
@@ -87,10 +65,12 @@ class Appointments extends Model implements Event {
      *
      * @return string
      */
-    public function getTitle() {
+    public function getTitle()
+    {
         if ($this->is_guest)
             return $this->guest . ' | ' . $this->doctors->full_name;
-        return /* $this->procedures->name . ' | ' . */ $this->patients->full_name . ' |' . $this->doctors->full_name;
+        return /* $this->procedures->name . ' | ' . */
+            $this->patients->full_name . ' |' . $this->doctors->full_name;
     }
 
     /**
@@ -98,7 +78,8 @@ class Appointments extends Model implements Event {
      *
      * @return bool
      */
-    public function isAllDay() {
+    public function isAllDay()
+    {
         return false;
     }
 
@@ -107,7 +88,8 @@ class Appointments extends Model implements Event {
      *
      * @return DateTime
      */
-    public function getStart() {
+    public function getStart()
+    {
         return $this->time;
     }
 
@@ -116,7 +98,8 @@ class Appointments extends Model implements Event {
      *
      * @return DateTime
      */
-    public function getEnd() {
+    public function getEnd()
+    {
         return (new Date($this->time))->add('30 minutes');
     }
 
@@ -125,7 +108,8 @@ class Appointments extends Model implements Event {
      *
      * @return array
      */
-    public function getEventOptions() {
+    public function getEventOptions()
+    {
         $color = 'black';
         switch ($this->status) {
             case 1:
@@ -143,7 +127,7 @@ class Appointments extends Model implements Event {
         return [
             //'color' => $color,
             'color' => get_color_code($this->category)
-                //etc
+            //etc
         ];
         /*
           1 => 'Scheduled',
